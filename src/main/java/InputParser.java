0a038045b063a85f03bcc3784674343dc049f3bd
wingedthing <jsfara@protonmail.com>
2022-01-29 10:22:00 -0500

    Create a new class to handle parsing of user input.

---
+++ src/main/java/InputParser.java
@@ -0,0 +1,28 @@
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputParser {
+
+    /**
+     * Parses a user input string and returns an Integer List that represents a
+     * graphical degree sequence.
+     *
+     * @param userInput  user input String.
+     * @return           the correctly formatted and parsed degree sequence.
+     * @throws Exception if input is non-numerical or empty.
+     */
+    public List<Integer> parse(String userInput) throws Exception{
+        List<Integer> degreeSequence = Stream.of(userInput.replaceAll("[^0-9 ]"," ").split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        if(degreeSequence.isEmpty()) {
+            throw new Exception("Invalid input");
+        }
+
+        return degreeSequence;
+    }
+
+}
